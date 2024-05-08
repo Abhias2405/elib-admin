f6ac81858e6582856f49fba2c4deea0e760b8b7f
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-05-08 23:39:00 +0530

    add loading state

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:5513",
+    baseURL: process.env.BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
