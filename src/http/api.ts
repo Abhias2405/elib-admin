e42b2a38a355b153a643ee345373a278793a433d
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-05-11 20:11:00 +0530

    add book functionality

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
+import useTokenStore from '@/store';
+
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
+});
+
+api.interceptors.request.use((config) => {
+    const token = useTokenStore.getState().token;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export const login = async (data: { email: string; password: string }) =>
@@ -14,3 +24,10 @@ export const register = async (data: { name: string; email: string; password: st
     api.post('/api/users/register', data);
 
 export const getBooks = async () => api.get('/api/books');
+
+export const createBook = async (data: FormData) =>
+    api.post('/api/books', data, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    });
